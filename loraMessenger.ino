45b0aa37c7ec70d55097afdff0dfe0e0e065ee6c
mkngrm <mchldwrdngrm@gmail.com>
2023-05-15 15:50:06 -0400

    Update loraMessenger.ino

--- loraMessenger.ino
+++ loraMessenger.ino
@@ -1,7 +1,6 @@
 /* TO DO:
  *  Add battery percentage
  *  Setup sleep mode for extended use
- *  Refine tx power settings
  *  Fix input character deletion
  *  Fix extra characters in receive buffer
  *  Display signal strength of received messages
@@ -10,7 +9,12 @@
  *  Speaker to alert on message receipt
  *  Send ack
  *    Resend until message acked
- *  Change screen/keyboard brightness based on ambient light
+ *  Change screen/keyboard brightness based on ambient light -or- use keyboard buttons to increment/decrement
+ *  Send broadcast when user comes online
+ *  Shutoff when battery voltage too low
+ *  Use keyboard button to ping users in range
+ *  
+ *  WHEN COMPLETE, BUMP UP TX POWER LEVEL
  */
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -68,6 +72,7 @@ Adafruit_ILI9341 tft(TFT_CS, TFT_DC);
 #define NEOPIXEL_PIN 11
 Adafruit_NeoPixel neopixel(1, NEOPIXEL_PIN, NEO_GRB + NEO_KHZ800);
 bool neopixel_flash = false;
+int NEOPIXEL_BRIGHTNESS = 16; 
 
 const uint16_t STATUS_BAR_COLOR = ILI9341_WHITE;
 const uint16_t STATUS_BAR_TEXT_COLOR = ILI9341_BLACK;
@@ -90,15 +95,12 @@ int inputBufferIndex = 0;
 // Message array
 String messageArray[255];
 int messageArrayIndex = 0;
-//String messageArray[255] = { "Message 1","Message 2", "Message 3" };
-//int messageArrayIndex = 2;
 
+float KEYBOARD_BACKLIGHT = 1.0;
 BBQ10Keyboard keyboard;
 
 //#define DEVICE_NAME "myMessenger"
 char DEVICE_NAME[INPUT_BUFFER_SIZE]; 
-
-int batteryPercentage;
 
 void setup() //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 {
@@ -123,15 +125,17 @@ void setup() ///////////////////////////////////////////////////////////////////
 
   // Initialize Neopixel
   neopixel.begin();
+  neopixel.setBrightness(NEOPIXEL_BRIGHTNESS);
   //neopixel.show(); // Turn off neopixel
   
   keyboard.begin();
-
+  keyboard.setBacklight(KEYBOARD_BACKLIGHT); // Turn on keyboard backlight
+  
   updateScreen(); 
   setUsername();
   
   Serial.println("Setup complete, beginning program.");
-  flashNeopixel(255, 255, 255, 10);
+  flashNeopixel(255, 255, 255, 1);
 }
 
 void loop() ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -147,14 +151,16 @@ void loop() ////////////////////////////////////////////////////////////////////
       if (rf95.recv(buf, &len)) {
         Serial.println("  Message received!");
         // Flash neopixel red
-        flashNeopixel(255, 0, 0, 127);
+        flashNeopixel(255, 0, 0, 1);
         char* receivedMessage = ((char*) buf);
         char formattedMessage[256];
+        
+        // Fix RSSI & SNR display
         int rssi = (rf95.lastRssi(), DEC);
         Serial.print(" Last RSSI: "); Serial.println(rf95.lastRssi());
         Serial.print(" Last SNR: "); Serial.println(rf95.lastSNR());
         
-        snprintf(formattedMessage, sizeof(formattedMessage), "%s (%s)", receivedMessage, (rf95.lastRssi(), DEC)); 
+        snprintf(formattedMessage, sizeof(formattedMessage), "%s (%s)", receivedMessage, (String) rf95.lastRssi()); 
 
         messageArray[messageArrayIndex] = formattedMessage;
         messageArrayIndex++;
@@ -184,6 +190,57 @@ void loop() ////////////////////////////////////////////////////////////////////
           sendLoRaMessage((char*) inputBuffer);
           break; 
         }
+        else if (key.key == 6) { // Keyboard button 1 - Ping local users to see who's available
+          
+        }
+        else if (key.key == 17) { // Keyboard button 2 - 
+          
+        }
+        else if (key.key == 1) { // Joystick Up - 
+          
+        }
+        else if (key.key == 2) { // Joystick Down - 
+          
+        }
+        else if (key.key == 3) { // Joystick Left - 
+          
+        }
+        else if (key.key == 4) { // Joystick Right - 
+          
+        }
+        else if (key.key == 5) { // Joystick Click - 
+          
+        }
+        else if (key.key == 7) { // Keyboard button 3 - Decrement keyboard brightness by 10%
+          if (KEYBOARD_BACKLIGHT > 0) {
+            KEYBOARD_BACKLIGHT -= .25;
+            keyboard.setBacklight(KEYBOARD_BACKLIGHT);
+            if (KEYBOARD_BACKLIGHT == 0) {
+              tft.writeCommand(0x10); // Sleep
+              delay(5); // Delay for shutdown time before another command can be sent
+            }
+          }
+        }
+        else if (key.key == 18) { // Keyboard button 4 - Increment keyboard brightness by 10%
+          if (KEYBOARD_BACKLIGHT < 1.0) {
+            if (KEYBOARD_BACKLIGHT == 0) {
+              tft.writeCommand(0x11); // Wake display
+              delay(120); // Delay for pwer supplies to stabilise
+            }            
+            KEYBOARD_BACKLIGHT += .25;
+            keyboard.setBacklight(KEYBOARD_BACKLIGHT);
+            
+          }          
+        }
+        else { // Unmapped key pressed, output value to determine usability
+          Serial.printf("key: '%c' (dec %d, hex %02x)\r\n", key.key, key.key, key.key);
+        }
+      }
+      else if (key.state == BBQ10Keyboard::StateLongPress) { // Additional functionality with a long press
+        
+      }      
+      else { // Additional functionality with a short press
+        
       }
     }
   }
@@ -224,14 +281,14 @@ void setUsername() /////////////////////////////////////////////////////////////
   clearInputBuffer();
   updateScreen(); 
   
-  Serial.println("Completing setUsername()");
+  Serial.println(" Completing setUsername()");
 }
 
 void drawStatusBar() //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 {
   Serial.println("Calling drawStatusBar()");
 
-  // Draw status bar at the top
+  // Draw status bar at the top and print device name
   tft.fillRect(0, 0, TFT_WIDTH, STATUS_BAR_HEIGHT, STATUS_BAR_COLOR);
   tft.setTextColor(STATUS_BAR_TEXT_COLOR);
   tft.setTextSize(1);
@@ -239,11 +296,21 @@ void drawStatusBar() ///////////////////////////////////////////////////////////
   tft.print(DEVICE_NAME);
   
   // TO FIX: Calculate battery percentage            
-  tft.setCursor(TFT_WIDTH - 30, 2);
-  float vBat = readVoltage();
-  tft.print(vBat); tft.print("V");
+  tft.setCursor(TFT_WIDTH - 25, 2);
+  int batteryPercent = readVoltage();
+  /*if (batteryPercent <= 40) {
+    tft.setTextColor(ILI9341_YELLOW);
+  }
+  else if (batteryPercent <= 10) {
+    tft.setTextColor(ILI9341_RED);
+  }
+  else {
+    tft.setTextColor(ILI9341_GREEN);
+  }*/
+
+  tft.printf("%3d", batteryPercent); tft.print("%");
   
-  Serial.println("Completing drawStatusBar()");
+  Serial.println(" Completing drawStatusBar()");
 }
 
 void drawMessageArea() ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -268,7 +335,7 @@ void drawMessageArea() /////////////////////////////////////////////////////////
     messagesDisplayed++;
   }
 
-  Serial.println("Completing drawMessageArea()");
+  Serial.println(" Completing drawMessageArea()");
 }
 
 void drawInputBar(const char* text) ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -283,7 +350,7 @@ void drawInputBar(const char* text) ////////////////////////////////////////////
   tft.setCursor(5, (TFT_HEIGHT - INPUT_BAR_HEIGHT + 1));
   tft.print(text);
 
-  Serial.println("Completing drawInputBar()");
+  Serial.println(" Completing drawInputBar()");
 }
 
 void sendLoRaMessage(const char* text) ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -317,8 +384,8 @@ void sendLoRaMessage(const char* text) /////////////////////////////////////////
 
 void sendAck() ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 {
-  uint8_t data[] = "*";
-  rf95.send(data, sizeof(data));
+  uint8_t ack[] = "*";
+  rf95.send(ack, sizeof(ack));
   //rf95.waitPacketSent();
 }
 
@@ -333,64 +400,51 @@ void updateScreen() ////////////////////////////////////////////////////////////
   Serial.println(" Completing updateScreen()");
 }
 
-void flashNeopixel(uint8_t r, uint8_t g, uint8_t b, uint8_t bright) ///////////////////////////////////////////////////////////////////////////////////////////////////////
+void flashNeopixel(uint8_t r, uint8_t g, uint8_t b, uint8_t count) ///////////////////////////////////////////////////////////////////////////////////////////////////////
 {
   Serial.println("Calling flashNeoPixel()");
-  
-  neopixel.setPixelColor(0, r, g, b, bright);
-  neopixel.show();
-  delay(50);
-  neopixel.setPixelColor(0, 0, 0, 0);
-  neopixel.show();
-  delay(50);
 
+  for (int i = 0; i < count; i++) {
+    neopixel.setPixelColor(0, r, g, b);
+    neopixel.show();
+    delay(50);
+    neopixel.setPixelColor(0, 0, 0, 0);
+    neopixel.show();
+    delay(50);
+  }
   Serial.println(" Completing flashNeoPixel()");
 }
 
 void clearInputBuffer() ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 {
+  Serial.println("Calling clearInputBUffer()");
+
   for(int i = 0; i <= inputBufferIndex; i++) {
     inputBuffer[i] = 0;    
   }
   inputBufferIndex = 0;
   drawInputBar(inputBuffer);
+
+  Serial.println(" inputBuffer Cleared");  
 }
 
-float readVoltage() ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+int readVoltage() /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 {
-  float measuredvbat = analogRead(VBATPIN);
-  measuredvbat *= 2;    // we divided by 2, so multiply back
-  measuredvbat *= 3.3;  // Multiply by 3.3V, our reference voltage
-  measuredvbat /= 1024; // convert to voltage
+  Serial.println("Calling readVoltage()"); 
+   
+  float voltageLevel = (analogRead(VBATPIN) * 2 * 3.3 / 1024) - 2; 
+  int batteryPercent = (int) ((voltageLevel / 4.2) * 100);
 
   tft.begin();
   tft.setRotation(1);
-
-  return measuredvbat;
-
-  /*float vPow = 3.3;
-  float r1 = 100000;
-  float r2 = 10000;
-
-  float v = (analogRead(A7) * vPow) / 1024.0;
-  //float v = (475 * vPow) / 1024.0;
-  float v2 = v / (r2 / (r1 + r2));
   
-  float top = 4.2;
-  float bottom = 3.6;
-  float range = top - bottom;
-  float rangeVolts = v2 - bottom;
-  
-  int percent = (rangeVolts / range) * 100;
-  if(percent > 100){
-    percent = 100;
-  }
-  if(percent < 0) {
-    percent = 0;
-  }
+  return batteryPercent;
+  Serial.println(" Completing readVoltage()");  
+}
 
-  
-  Serial.print("  Battery: ");
-  Serial.print(percent); Serial.println("%");
-  return percent;*/
+void setBacklight() ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+{
+  // Read current ambient light
+
+  // Calculate 
 }
